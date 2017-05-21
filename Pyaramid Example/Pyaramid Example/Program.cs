8fe77ef422d6c5c9130ac94f34d643b9e68af3dc
trivedikaran <trivedikaran13@gmail.com>
2017-05-21 12:31:23 +0530

    Pyramid Example

---
+++ Pyaramid Example/Pyaramid Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyaramid_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number : ");
+            int numberOfLines = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                for (int a = numberOfLines - 1; a >= i; a--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
